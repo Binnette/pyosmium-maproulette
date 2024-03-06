6815d69f298de17b70d288de930f7ddfcd0326f3
Binnette <binnette@gmail.com>
2024-03-06 10:14:37 +0100

    Add museum-without-website.py

--- museum-without-website.py
+++ museum-without-website.py
@@ -1,18 +1,17 @@
 """
-This script store museum without fee in a osm file.
+This script store place of worship without relegion in a osm file.
 
 1. Run the script
 2. Open the osm file with JOSM
 3. Update data
-4. Ctrl+F with filter tourism=museum -fee
+4. Ctrl+F with filter amenity=place_of_worship -religion
 5. Create a new layer
-6. Merge selection to new layer(Ctrl+Shift+M)
+6. Merge selection to nex layer(Ctrl+Shift+M)
 7. save the new layer as geojson
 
 Stats for planet-220818.osm.pbf
-- Nodes found: 45259
-- Ways found: 37620
-- Program ended in 12:43:15.36
+-Nodes found: 35199 - Ways found: 29882
+-Program ended in 17:22:57.81
 
 """
 import os
@@ -22,16 +21,16 @@ import getopt
 import time
 
 
-class MuseumWithoutFee(osmium.SimpleHandler):
+class MuseumWithoutWebsite(osmium.SimpleHandler):
 
     def __init__(self, writer):
-        super(MuseumWithoutFee, self).__init__()
+        super(MuseumWithoutWebsite, self).__init__()
         self.writer = writer  # osmium writer
         self.nbWay = 0  # counter ways
         self.nbNode = 0  # counter nodes
 
     def isConcerned(self, obj):
-        return obj.tags.get('tourism') == 'museum' and 'fee' not in obj.tags
+        return obj.tags.get('tourism') == 'museum' and 'website' not in obj.tags
 
     # osmium way handler
     def node(self, n):
@@ -63,11 +62,11 @@ class MuseumWithoutFee(osmium.SimpleHandler):
 def print_help():
     print("Usage: python %s -i <osmfile> -o <output.geojson>" % sys.argv[0])
     print("")
-    print("Read the <osmfile> in input. Write a geojson file of found objects.")
+    print("Read the <osmfile> in input. Find place of worship without religion. Write a geojson file.")
     print("Use this geojson to create a MapRoulette challenge.")
     print("")
     print("  -i <input osm file> such as planet.osm.pbf. All file supported by osmium should work")
-    print("  -o <output filename.geojson>. A file containing found objects")
+    print("  -o <output filename.geojson>. A file to write the Overpass query inside")
     print("", flush=True)
     exit()
 
@@ -81,7 +80,7 @@ def main(input, output):
         print("Initialize writer", flush=True)
         writer = osmium.SimpleWriter(output)
         print("Initialize handler", flush=True)
-        handler = MuseumWithoutFee(writer)
+        handler = MuseumWithoutWebsite(writer)
         print("Start handler...", flush=True)
         handler.apply_file(input, locations=False)
         writer.close()
@@ -104,7 +103,7 @@ if __name__ == '__main__':
 
     # default arguments values
     input = ""
-    output = "out/museum.osm"
+    output = "out/place-of-worship.osm"
 
     # parse arguments
     opts, args = getopt.getopt(sys.argv[1:], "i:o", ["input =", "output ="])
